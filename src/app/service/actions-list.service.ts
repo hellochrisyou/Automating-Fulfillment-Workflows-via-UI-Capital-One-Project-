0ef71099d7c2e572c1c1b13924299e8dbb9a9483
Chris You <chris.you@sagemi.com>
2022-04-08 20:51:32 -0400

    error fix

--- src/app/service/actions-list.service.ts
+++ src/app/service/actions-list.service.ts
@@ -31,35 +31,44 @@ export class ActionsListService {
 
   async removeAction(financeList: FinanceList) {
     // optimistic update
-    this.actionsList = this.actionsList.filter(list => list.name !== name);
+    this.actionsList = this.actionsList.filter(list => list.name !== financeList.name);
   }
 
   async fetchAll() {
     this.actionsList = [
-      { name: 'Change Rewards',
+      {
+        name: 'Change Rewards',
         description: 'This action will change the rewards for the given account'
       },
-      { name: 'Change Network Product',
+      {
+        name: 'Change Network Product',
         description: 'This action will change the network for the given account by just using the destination Opset ID'
       },
-      { name: 'Change Membership Fee Details',
+      {
+        name: 'Change Membership Fee Details',
         description: 'This action will change the fee for the given account'
       },
-      { name: 'Send Letter',
+      {
+        name: 'Send Letter',
         description: 'This action will send a reward letter to the given account holder'
       },
-      { name: 'Change Credit Line',
+      {
+        name: 'Change Credit Line',
         description: 'This action will perform a CLIP for the given account'
       },
-      { name: 'Transfer Balance',
+      {
+        name: 'Transfer Balance',
         description: 'This action will initiate a balance transfer'
       },
-      { name: 'Change Product Terms',
+      {
+        name: 'Change Product Terms',
         description: 'This action will change the product for the given account'
       },
-      { name: 'Change Venture Membership Fee Details',
-        description: 'This action will change the fee for the given account'  
+      {
+        name: 'Change Venture Membership Fee Details',
+        description: 'This action will change the fee for the given account'
       }
-  ]  };
+    ]
+  };
 
 }
